28fd8566348da91e64e59d707842bbce2a274950
pmohapatra <biswapm@users.noreply.github.com>
2026-04-14 11:40:00 +0530

    fix: switch ProductionDiscoverEndpointUrl to production endpoint

--- src/Microsoft.Agents.A365.DevTools.Cli/Constants/ConfigConstants.cs
+++ src/Microsoft.Agents.A365.DevTools.Cli/Constants/ConfigConstants.cs
@@ -36,8 +36,8 @@ public static class ConfigConstants
     /// <summary>
     /// Agent 365 Tools Discover endpoint URL (V2)
     /// </summary>
-    public const string ProductionDiscoverEndpointUrl = "https://test.agent365.svc.cloud.dev.microsoft/agents/v2/discoverMCPServers";
-  //public const string ProductionDiscoverEndpointUrl = "https://agent365.svc.cloud.microsoft/agents/v2/discoverMCPServers";
+
+   public const string ProductionDiscoverEndpointUrl = "https://agent365.svc.cloud.microsoft/agents/v2/discoverMCPServers";
 
     /// <summary>
     /// Production Agent 365 Tools Create endpoint URL
